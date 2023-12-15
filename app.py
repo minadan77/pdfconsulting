1b1d05022d3f579834701b9b48bd58f67c193c8c
minadan77 <96655942+minadan77@users.noreply.github.com>
2023-12-15 18:18:46 +0000

    app.py

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ app = Flask(__name__)
 
 model = transformers.T5ForConditionalGeneration.from_pretrained("t5-base")
 
-@app.route("/upload")
+@app.route("/upload", methods=["POST", "GET"])
 def upload():
-  pdf = request.files["pdf"]
+  if request.method == "POST":
+    pdf = request.files["pdf"]
 
-  pdf_content = pdf.read()
+    pdf_content = pdf.read()
 
-  response = model.generate(input_text=pdf_content)
+    response = model.generate(input_text=pdf_content)
 
-  question = response[0]["input_text"]
-  answer = response[0]["output_text"]
+    question = response[0]["input_text"]
+    answer = response[0]["output_text"]
 
-  return render_template("index.html", question=question, answer=answer)
+    return render_template("index.html", question=question, answer=answer)
+  else:
+    return "Solo se permite el método POST"
 
 if __name__ == "__main__":
   app.run()
