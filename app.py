d8e896c084c6aad01f43183829a23bfec53aad88
minadan77 <96655942+minadan77@users.noreply.github.com>
2023-12-15 18:14:49 +0000

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-import bard
+import transformers
 
-app = bard.App()
+app = Flask(__name__)
 
-def query(pdf):
-  response = app.query(pdf)
-  return response
+model = transformers.T5ForConditionalGeneration.from_pretrained("t5-base")
+
+@app.route("/upload")
+def upload():
+  pdf = request.files["pdf"]
+
+  pdf_content = pdf.read()
+
+  response = model.generate(input_text=pdf_content)
+
+  question = response[0]["input_text"]
+  answer = response[0]["output_text"]
+
+  return render_template("index.html", question=question, answer=answer)
 
 if __name__ == "__main__":
   app.run()
